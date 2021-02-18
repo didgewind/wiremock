525d489ee29bff92ef5b3882fe616b0fb5bf516d
made <kike@mazarredos.com>
2021-02-18 22:16:49 +0100

    WireMock lanzado como proceso desde el main
    No necesitamos lanzar wiremock primero y el sidecar a continuación, se
    lanza todo secuencialmente en el main de la app spring boot

--- src/main/java/profe/empleados/wiremock/EmpleadosWireMockSidecar.java
+++ src/main/java/profe/empleados/wiremock/EmpleadosWireMockSidecar.java
@@ -1,22 +1,31 @@
 package profe.empleados.wiremock;
 
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 
 @SpringBootApplication
 @EnableSidecar
 public class EmpleadosWireMockSidecar {
 	
 	public static void main(String[] args) {
+		
+		// Primero lanzamos el wiremock server
+		WireMockServer wireMockServer = 
+				new WireMockServer(
+						options()
+							.port(5555)
+							.usingFilesUnderDirectory("standalone server") // Usamos las mismas carpetas __files y mappings que el standaalone server
+							.extensions(new ResponseTemplateTransformer(true))); // permite usar variables {{ }} en las respuestas para todos los stubs (parámetro true)
+		wireMockServer.start();
+		
+		// Luego el sidecar
 		System.setProperty("spring.config.name", "empleados-wiremock-sidecar");
 		SpringApplication.run(EmpleadosWireMockSidecar.class, args);
 	}
